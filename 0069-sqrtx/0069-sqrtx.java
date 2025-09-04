a08237706c7718cfaf3a4937602eafdf3d45dafe
10pranjal2005 <pk5518@srmist.edu.in>
2025-09-05 02:49:53 +0530

    Time: 1 ms (84.22%), Space: 41.1 MB (30.28%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,18 +1,12 @@
-class Solution{
-    public int mySqrt(int x){
-        if (x<2){
-            return x;
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0) {
+            return 0;
         }
-        int left=1;
-        int right =x;
-        while (left<=right){
-            int mid =left+(right-left)/2;
-            if(mid>x/mid){
-                right=mid-1;
-            }else{
-                left=mid+1;
-            }
+        long r = x;
+        while (r > x / r) {
+            r = (r + x / r) / 2;
         }
-        return right;
+        return (int) r;
     }
 }
