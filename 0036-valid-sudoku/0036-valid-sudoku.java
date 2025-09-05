889d54cb71bc056d77a55ab3289f8b20f38d9e77
10pranjal2005 <pk5518@srmist.edu.in>
2025-09-05 13:08:26 +0530

    Time: 1 ms (99.62%), Space: 44.6 MB (55.75%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        boolean[][] rows = new boolean[9][9];
-        boolean[][] cols = new boolean[9][9];
-        boolean[][] boxes = new boolean[9][9];
+        int[] rows = new int[9];
+        int[] cols = new int[9];
+        int[] boxes = new int[9];
 
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
@@ -13,16 +13,23 @@ class Solution {
                 }
 
                 int num = currentVal - '1';
-                
-                int boxIndex = (i / 3) * 3 + (j / 3);
+                int mask = 1 << num;
 
-                if (rows[i][num] || cols[j][num] || boxes[boxIndex][num]) {
+                if ((rows[i] & mask) != 0) {
                     return false;
                 }
+                rows[i] |= mask;
 
-                rows[i][num] = true;
-                cols[j][num] = true;
-                boxes[boxIndex][num] = true;
+                if ((cols[j] & mask) != 0) {
+                    return false;
+                }
+                cols[j] |= mask;
+
+                int boxIndex = (i / 3) * 3 + (j / 3);
+                if ((boxes[boxIndex] & mask) != 0) {
+                    return false;
+                }
+                boxes[boxIndex] |= mask;
             }
         }
 
