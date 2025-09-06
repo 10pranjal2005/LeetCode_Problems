af7156034a90b19428a4b8136e95fd5dd2818f81
10pranjal2005 <pk5518@srmist.edu.in>
2025-09-06 12:32:35 +0530

    Time: 1 ms (99.87%), Space: 46.1 MB (13.64%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,19 +1,4 @@
-/*class Solution {
-    public boolean canJump(int[] nums) {
-        int jump = 0;
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            if (i > jump) {
-                return false;
-            }
-            jump = Math.max(jump, i + nums[i]);
-            if (jump >= n - 1) {
-                return true;
-            }
-        }
-        return true;
-    }
-}*/
+
 class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length;
