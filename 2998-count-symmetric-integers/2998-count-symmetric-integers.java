e3dc894883893372af3e5b607a49abfba82f6865
10pranjal2005 <pk5518@srmist.edu.in>
2025-09-07 01:08:58 +0530

    Time: 11 ms (93.21%), Space: 41.3 MB (94.35%) - LeetHub

--- 2998-count-symmetric-integers/2998-count-symmetric-integers.java
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.java
@@ -1,36 +1,34 @@
 /*
 class Solution {
     public int countSymmetricIntegers(int low, int high) {
-        int count = 0;
-        for (int i = low; i <= high; i++) {
+        int count=0;
+        for (int i=low;i<=high;i++) {
             if (isSymmetric(i)) {
                 count++;
             }
         }
         return count;
     }
-
     private boolean isSymmetric(int num) {
         String s = String.valueOf(num);
         int n = s.length();
-
-        if (n % 2 != 0) {
+        if (n%2!=0) {
             return false;
         }
 
-        int mid = n / 2;
-        int firstHalfSum = 0;
-        int secondHalfSum = 0;
+        int mid=n/2;
+        int Sum1=0;
+        int Sum2=0;
 
-        for (int i = 0; i < mid; i++) {
-            firstHalfSum += s.charAt(i) - '0'; //convert to int form @ [-'0']
+        for (int i=0;i<mid;i++) {
+            Sum1+=s.charAt(i) - '0'; //convert to int form @ [-'0']
         }
 
-        for (int i = mid; i < n; i++) {
-            secondHalfSum += s.charAt(i) - '0'; //convert to int form @ [-'0']
+        for (int i=mid;i<n;i++) {
+            Sum2+=s.charAt(i) - '0'; //convert to int form @ [-'0']
         }
 
-        return firstHalfSum == secondHalfSum;
+        return Sum1==Sum2;
     }
 }
 */
@@ -39,20 +37,20 @@ class Solution {
         int count = 0;
         for (int i = low; i <= high; i++) {
             if (i >= 10 && i <= 99) {
-                int firstDigit = i / 10;
-                int secondDigit = i % 10;
-                if (firstDigit == secondDigit) {
+                int digit1 = i / 10;
+                int digit2 = i % 10;
+                if (digit1 == digit2) {
                     count++;
                 }
             } 
             else if (i >= 1000 && i <= 9999) {
-                int firstHalf = i / 100;
-                int secondHalf = i % 100;
+                int half1 = i / 100;
+                int half2 = i % 100;
 
-                int firstHalfSum = (firstHalf / 10) + (firstHalf % 10);
-                int secondHalfSum = (secondHalf / 10) + (secondHalf % 10);
+                int sum1 = (half1 / 10) + (half1 % 10);
+                int sum2 = (half2 / 10) + (half2 % 10);
 
-                if (firstHalfSum == secondHalfSum) {
+                if (sum1 == sum2) {
                     count++;
                 }
             }
