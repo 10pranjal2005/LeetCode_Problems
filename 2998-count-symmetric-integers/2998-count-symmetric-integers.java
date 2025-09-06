ff11834216731b4613a37b8aa612fcf3e225e09f
10pranjal2005 <pk5518@srmist.edu.in>
2025-09-07 01:04:32 +0530

    Time: 10 ms (99.08%), Space: 41.4 MB (88.09%) - LeetHub

--- 2998-count-symmetric-integers/2998-count-symmetric-integers.java
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public int countSymmetricIntegers(int low, int high) {
         int count = 0;
@@ -30,5 +31,32 @@ class Solution {
         }
 
         return firstHalfSum == secondHalfSum;
+    }
+}
+*/
+class Solution {
+    public int countSymmetricIntegers(int low, int high) {
+        int count = 0;
+        for (int i = low; i <= high; i++) {
+            if (i >= 10 && i <= 99) {
+                int firstDigit = i / 10;
+                int secondDigit = i % 10;
+                if (firstDigit == secondDigit) {
+                    count++;
+                }
+            } 
+            else if (i >= 1000 && i <= 9999) {
+                int firstHalf = i / 100;
+                int secondHalf = i % 100;
+
+                int firstHalfSum = (firstHalf / 10) + (firstHalf % 10);
+                int secondHalfSum = (secondHalf / 10) + (secondHalf % 10);
+
+                if (firstHalfSum == secondHalfSum) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
