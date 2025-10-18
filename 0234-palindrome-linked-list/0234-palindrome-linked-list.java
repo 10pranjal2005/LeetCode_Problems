9c1b6c22124f36b9262108b27ddd6fa2bcf307e8
10pranjal2005 <pk5518@srmist.edu.in>
2025-10-18 19:29:36 +0530

    Time: 5 ms (54.03%), Space: 69.3 MB (9.18%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,35 +1,44 @@
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        List<Integer> values = new ArrayList<>();
-        ListNode currentNode = head;
-        while (currentNode != null) {
-            values.add(currentNode.val);
-            currentNode = currentNode.next;
+        if (head == null || head.next == null) {
+            return true;
         }
+
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode secondHalfHead = reverse(slow.next);
+        ListNode p1 = head;
+        ListNode p2 = secondHalfHead;
+        boolean isPalindrome = true;
         
-        int left = 0;
-        int right = values.size() - 1;
-        while (left < right) {
-            if (!values.get(left).equals(values.get(right))) {
-                return false;
+        while (p2 != null) {
+            if (p1.val != p2.val) {
+                isPalindrome = false;
+                break;
             }
-            left++;
-            right--;
+            p1 = p1.next;
+            p2 = p2.next;
         }
-        
-        return true;
+
+        slow.next = reverse(secondHalfHead);
+
+        return isPalindrome;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        return prev;
     }
 }
