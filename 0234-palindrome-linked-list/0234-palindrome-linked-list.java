b2cc3495a0c7f59c3e98407e4c93b8522b2091a0
10pranjal2005 <pk5518@srmist.edu.in>
2025-10-18 19:30:56 +0530

    Time: 11 ms (17.81%), Space: 75.2 MB (5.24%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,44 +1,16 @@
 class Solution {
+    private ListNode front;
+
     public boolean isPalindrome(ListNode head) {
-        if (head == null || head.next == null) {
-            return true;
-        }
-
-        ListNode slow = head;
-        ListNode fast = head;
-        while (fast.next != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        ListNode secondHalfHead = reverse(slow.next);
-        ListNode p1 = head;
-        ListNode p2 = secondHalfHead;
-        boolean isPalindrome = true;
-        
-        while (p2 != null) {
-            if (p1.val != p2.val) {
-                isPalindrome = false;
-                break;
-            }
-            p1 = p1.next;
-            p2 = p2.next;
-        }
-
-        slow.next = reverse(secondHalfHead);
-
-        return isPalindrome;
+        front = head;
+        return check(head);
     }
 
-    private ListNode reverse(ListNode head) {
-        ListNode prev = null;
-        ListNode current = head;
-        while (current != null) {
-            ListNode nextNode = current.next;
-            current.next = prev;
-            prev = current;
-            current = nextNode;
-        }
-        return prev;
+    private boolean check(ListNode node) {
+        if (node == null) return true;
+        if (!check(node.next)) return false;
+        if (front.val != node.val) return false;
+        front = front.next;
+        return true;
     }
 }
