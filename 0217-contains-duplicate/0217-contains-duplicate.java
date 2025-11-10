0285fb6da3e127a3b97b66879377b19725021d0b
10pranjal2005 <pk5518@srmist.edu.in>
2025-11-10 10:53:41 +0530

    Time: 14 ms (63.17%), Space: 93.5 MB (5.69%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        
+        for (int num : nums) {
+            // If the number already exists in the set → duplicate found
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        
+        // No duplicates found
+        return false;
+    }
+}
