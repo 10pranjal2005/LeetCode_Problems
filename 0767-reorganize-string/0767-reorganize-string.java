6eeccaaec3788936ec8baec69531ac4db109696c
10pranjal2005 <pk5518@srmist.edu.in>
2025-12-27 14:09:15 +0530

    Time: 3 ms (73.52%), Space: 43.1 MB (36.25%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public String reorganizeString(String s) {
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) freq[c - 'a']++;
+
+        PriorityQueue<int[]> pq =
+            new PriorityQueue<>((a, b) -> b[1] - a[1]);
+
+        for (int i = 0; i < 26; i++)
+            if (freq[i] > 0)
+                pq.add(new int[]{i, freq[i]});
+
+        StringBuilder sb = new StringBuilder();
+        int[] prev = null;
+
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            sb.append((char)(cur[0] + 'a'));
+            cur[1]--;
+
+            if (prev != null && prev[1] > 0)
+                pq.add(prev);
+
+            prev = cur;
+        }
+
+        return sb.length() == s.length() ? sb.toString() : "";
+    }
+}
