34a5f5c64450bfb60e85e192b91c7af9193bf934
10pranjal2005 <pk5518@srmist.edu.in>
2025-12-28 20:01:26 +0530

    Time: 3 ms (34.46%), Space: 49.3 MB (6.53%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,27 +1,25 @@
 import java.util.*;
 
 class Solution {
-    List<String> res = new ArrayList<>();
-    String[] map = {
-        "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
-    };
-
     public List<String> letterCombinations(String digits) {
+        List<String> res = new ArrayList<>();
         if (digits.length() == 0) return res;
-        backtrack(digits, 0, new StringBuilder());
-        return res;
-    }
 
-    void backtrack(String digits, int idx, StringBuilder cur) {
-        if (idx == digits.length()) {
-            res.add(cur.toString());
-            return;
+        String[] map = {
+            "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+        };
+
+        res.add("");
+        for (char d : digits.toCharArray()) {
+            List<String> temp = new ArrayList<>();
+            String letters = map[d - '0'];
+            for (String s : res) {
+                for (char c : letters.toCharArray()) {
+                    temp.add(s + c);
+                }
+            }
+            res = temp;
         }
-        String letters = map[digits.charAt(idx) - '0'];
-        for (char c : letters.toCharArray()) {
-            cur.append(c);
-            backtrack(digits, idx + 1, cur);
-            cur.deleteCharAt(cur.length() - 1);
-        }
+        return res;
     }
 }
