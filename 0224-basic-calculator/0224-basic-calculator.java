b1d670e5d9b10520d32e947f4e60429e5833e854
10pranjal2005 <pk5518@srmist.edu.in>
2026-01-07 18:49:35 +0530

    Time: 12 ms (42.94%), Space: 46.6 MB (45.43%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int calculate(String s) {
+        int res = 0, num = 0, sign = 1;
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(c)) {
+                num = num * 10 + (c - '0');
+            } else if (c == '+') {
+                res += sign * num;
+                num = 0;
+                sign = 1;
+            } else if (c == '-') {
+                res += sign * num;
+                num = 0;
+                sign = -1;
+            } else if (c == '(') {
+                stack.push(res);
+                stack.push(sign);
+                res = 0;
+                sign = 1;
+            } else if (c == ')') {
+                res += sign * num;
+                num = 0;
+                res *= stack.pop();
+                res += stack.pop();
+            }
+        }
+        return res + sign * num;
+    }
+}
