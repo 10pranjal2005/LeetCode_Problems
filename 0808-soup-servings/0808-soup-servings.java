0c14409b4c6f25a32cf1dbc29974317157e05085
10pranjal2005 <pk5518@srmist.edu.in>
2026-01-22 23:13:57 +0530

    Time: 0 ms (100%), Space: 46 MB (70%) - LeetHub

---
+++ 0808-soup-servings/0808-soup-servings.java
@@ -0,0 +1,29 @@
+class Solution {
+    Double[][] dp;
+
+    public double soupServings(int n) {
+        if (n >= 4800) return 1.0;
+
+        int m = (n + 24) / 25;
+        dp = new Double[m + 1][m + 1];
+        return solve(m, m);
+    }
+
+    double solve(int a, int b) {
+        if (a <= 0 && b <= 0) return 0.5;
+        if (a <= 0) return 1.0;
+        if (b <= 0) return 0.0;
+
+        if (dp[a][b] != null) return dp[a][b];
+
+        dp[a][b] =
+            0.25 * (
+                solve(a - 4, b) +
+                solve(a - 3, b - 1) +
+                solve(a - 2, b - 2) +
+                solve(a - 1, b - 3)
+            );
+
+        return dp[a][b];
+    }
+}
