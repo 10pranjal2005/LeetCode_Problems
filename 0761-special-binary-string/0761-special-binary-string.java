5ebd3074ee678cd6fbf32b8d9f58a0348dcc1d31
10pranjal2005 <pk5518@srmist.edu.in>
2026-02-20 16:03:22 +0530

    Time: 2 ms (100%), Space: 43.1 MB (79.62%) - LeetHub

--- 0761-special-binary-string/0761-special-binary-string.java
+++ 0761-special-binary-string/0761-special-binary-string.java
@@ -1,31 +1,19 @@
-import java.util.*;
-
 class Solution {
     public String makeLargestSpecial(String s) {
         if (s.length() <= 2) return s;
-        
         List<String> list = new ArrayList<>();
         int count = 0;
         int start = 0;
-        
-        // Split into primitive special substrings
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '1') count++;
             else count--;
-            
-            // When balanced, we found one special substring
             if (count == 0) {
-                // Recursively process inner substring
                 String inner = makeLargestSpecial(s.substring(start + 1, i));
                 list.add("1" + inner + "0");
                 start = i + 1;
             }
         }
-        
-        // Sort in descending order to make lexicographically largest
         Collections.sort(list, Collections.reverseOrder());
-        
-        // Combine all
         StringBuilder result = new StringBuilder();
         for (String str : list) {
             result.append(str);
