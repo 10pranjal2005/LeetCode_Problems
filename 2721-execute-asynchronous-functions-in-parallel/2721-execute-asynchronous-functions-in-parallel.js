cee89424544e401673cf3cbcf9e5bd534d3db236
10pranjal2005 <pk5518@srmist.edu.in>
2026-04-25 11:16:55 +0530

    Time: 62 ms (59.73%), Space: 54.5 MB (63.83%) - LeetHub

---
+++ 2721-execute-asynchronous-functions-in-parallel/2721-execute-asynchronous-functions-in-parallel.js
@@ -0,0 +1,28 @@
+var promiseAll = function(functions) {
+
+    return new Promise((resolve, reject) => {
+
+        let results = new Array(functions.length);
+        let completed = 0;
+
+        if (functions.length === 0) {
+            resolve(results);
+            return;
+        }
+
+        functions.forEach((fn, index) => {
+
+            fn()
+                .then(value => {
+
+                    results[index] = value;
+                    completed++;
+
+                    if (completed === functions.length) {
+                        resolve(results);
+                    }
+                })
+                .catch(error => reject(error));
+        });
+    });
+};
