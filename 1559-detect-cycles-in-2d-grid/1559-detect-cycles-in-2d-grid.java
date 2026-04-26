a165ef0cdd97682c78f689b3e9733cbea41e7d89
10pranjal2005 <pk5518@srmist.edu.in>
2026-04-26 10:48:37 +0530

    Time: 18 ms (80.48%), Space: 140 MB (56.68%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
@@ -1,26 +1,24 @@
 class Solution{
-    public boolean containsCycle(char[][] grid){
-        int m=grid.length,n=grid[0].length;
-        boolean[][] vis=new boolean[m][n];
+    int[] d={1,0,-1,0,1};
+    public boolean containsCycle(char[][] g){
+        int m=g.length,n=g[0].length;
+        boolean[][] v=new boolean[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(!vis[i][j]){
-                    if(dfs(grid,vis,i,j,-1,-1,grid[i][j]))return true;
+                if(!v[i][j]){
+                    if(dfs(g,v,i,j,-1,-1,g[i][j]))return true;
                 }
             }
         }
         return false;
     }
-    boolean dfs(char[][] grid,boolean[][] vis,int r,int c,int pr,int pc,char ch){
-        if(vis[r][c])return true;
-        vis[r][c]=true;
-        int[][] d={{1,0},{-1,0},{0,1},{0,-1}};
-        for(int[] k:d){
-            int nr=r+k[0],nc=c+k[1];
-            if(nr>=0&&nc>=0&&nr<grid.length&&nc<grid[0].length&&grid[nr][nc]==ch){
-                if(nr==pr&&nc==pc)continue;
-                if(dfs(grid,vis,nr,nc,r,c,ch))return true;
-            }
+    boolean dfs(char[][] g,boolean[][] v,int r,int c,int pr,int pc,char ch){
+        v[r][c]=true;
+        for(int k=0;k<4;k++){
+            int nr=r+d[k],nc=c+d[k+1];
+            if(nr<0||nc<0||nr>=g.length||nc>=g[0].length||g[nr][nc]!=ch)continue;
+            if(nr==pr&&nc==pc)continue;
+            if(v[nr][nc]||dfs(g,v,nr,nc,r,c,ch))return true;
         }
         return false;
     }
