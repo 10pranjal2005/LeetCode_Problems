404cc9cb38c9ef0ab29b6a6ac4e1bd6000b42833
10pranjal2005 <pk5518@srmist.edu.in>
2026-07-21 19:29:27 +0530

    Time: 166 ms (14.08%), Space: 48.6 MB (25.35%) - LeetHub

---
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int maxActiveSectionsAfterTrade(String s) {
+        int ones = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '1') ones++;
+        }
+
+        String t = "1" + s + "1";
+
+        List<Character> type = new ArrayList<>();
+        List<Integer> len = new ArrayList<>();
+
+        int i = 0;
+        while (i < t.length()) {
+            char c = t.charAt(i);
+            int j = i;
+            while (j < t.length() && t.charAt(j) == c) {
+                j++;
+            }
+            type.add(c);
+            len.add(j - i);
+            i = j;
+        }
+
+        int gain = 0;
+
+        for (int k = 1; k < type.size() - 1; k++) {
+            if (type.get(k) == '1'
+                    && type.get(k - 1) == '0'
+                    && type.get(k + 1) == '0') {
+
+                gain = Math.max(gain, len.get(k - 1) + len.get(k + 1));
+            }
+        }
+
+        return ones + gain;
+    }
+}
