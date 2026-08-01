95fc6381b6d3aaa46145a46483c000679cafbf28
10pranjal2005 <pk5518@srmist.edu.in>
2026-08-01 21:23:48 +0530

    Time: 0 ms (100%), Space: 42.5 MB (94.79%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    int[][] dp;
+
+    public boolean predictTheWinner(int[] nums) {
+
+        int n = nums.length;
+        dp = new int[n][n];
+
+        for (int[] row : dp)
+            java.util.Arrays.fill(row, Integer.MIN_VALUE);
+
+        return solve(nums, 0, n - 1) >= 0;
+    }
+
+    int solve(int[] nums, int l, int r) {
+
+        if (l == r) return nums[l];
+
+        if (dp[l][r] != Integer.MIN_VALUE)
+            return dp[l][r];
+
+        int left = nums[l] - solve(nums, l + 1, r);
+        int right = nums[r] - solve(nums, l, r - 1);
+
+        return dp[l][r] = Math.max(left, right);
+    }
+}
